0aed6ffcd29040c89b54ed58f14a8879c9f316d0
DESKTOP-6P8KEUV\user <dbehdgjs97@naver.com>
2021-08-24 17:49:56 +0900

    2021-08-24

---
+++ src/main/java/com/karinagram/timeline/TimelineController.java
@@ -0,0 +1,5 @@
+package com.karinagram.timeline;
+
+public class TimelineController {
+
+}
---
+++ src/main/java/com/karinagram/timeline/domain/Content.java
@@ -0,0 +1,14 @@
+package com.karinagram.timeline.domain;
+
+public class Content {
+   //post - 1개
+	//private Post post;
+	//post - 좋아요 n개
+	//private List<like> likeList;
+	//post - 댓글 n개
+	//private List<Comment> commentList;
+	//post - 좋아요 개수
+	//private int likeCount;
+	
+	//private boolean isFilledLike;  // userId (나) - post-좋아요
+}
